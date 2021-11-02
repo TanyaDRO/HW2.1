4cc771a6aa0bdaf634b7e90cf86dc89dcb579ac9
TanyaDRO <drtanja@yandex.ru>
2021-11-02 16:47:40 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 1_000;
+        int mile = 20;
+        int bonus = ticket / mile;
+        System.out.println(bonus);
+    }
+}
